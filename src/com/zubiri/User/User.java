f4ae7535cac0b413306124d879dce50f45a19fa5
XArtola <xartolaza18dw@ikzubirimanteo.com>
2019-01-14 17:51:58 +0100

    I have fixed the problem with the password validation

--- src/com/zubiri/User/User.java
+++ src/com/zubiri/User/User.java
@@ -5,12 +5,16 @@ public class User {
 	private String username = "UserName";
 	private String password = "1234abcd=";
 
+	/**
+	 * There is a default user (UserName)
+	 * With a default password (1234abcd=)
+	 */
 	public User() {
 	}
 
 	public void setUsername(String username) {
 
-		if (!username.matches("[a-zA-Z+]")) {
+		if (!username.matches("[a-zA-Z]+")) {
 
 		}
 
@@ -29,16 +33,10 @@ public class User {
 
 	public void setPassword(String password) {
 
-		if (password.matches("[a-zA-Z+]") && password.matches("[0-9+]") && !password.matches("[a-zA-Z0-9+]")
-				&& password.length() > 7) {
+		if (!password.matches("[a-zA-Z]+") && !password.matches("[0-9]+") && !password.matches("[a-zA-Z0-9]+")
+				&& password.length() > 7)
 
 			this.password = password;
-
-		}
-
-		else {
-
-		}
 
 	}
 
@@ -47,6 +45,13 @@ public class User {
 		return password;
 
 	}
+	
+	/**
+	 * 
+	 * @param String username. The name that the user is going to have
+	 * @param String password. The password that the user is going to have
+	 * @return  -2: invalid username, -1: invalid password, 0: succesfully registered, 1: invalid username and password
+	 */
 
 	public int register(String username, String password) {
 
@@ -80,12 +85,18 @@ public class User {
 		return valid;
 
 	}
+	
+	/**
+	 * 
+	 * @param String username: the username to check if it is valid to create a user
+	 * @return boolean true: valid, false, invalid
+	 */
 
 	public boolean validUsername(String username) {
 
 		boolean validUserneme = true;
 
-		if (!username.matches("[a-zA-Z+]")) {
+		if (!username.matches("[a-zA-Z]+")) {
 
 			validUserneme = false;
 
@@ -94,12 +105,18 @@ public class User {
 		return validUserneme;
 
 	}
+	
+	/**
+	 * 
+	 * @param String password: the password to check if it is valid to create a user
+	 * @return boolean true: valid, false, invalid
+	 */
 
 	public boolean validPassword(String password) {
 
 		boolean validPassword = false;
 
-		if (password.matches("[a-zA-Z+]") && password.matches("[0-9+]") && !password.matches("[a-zA-Z0-9+]")
+		if (!password.matches("[a-zA-Z]+") && !password.matches("[0-9]+") && !password.matches("[a-zA-Z0-9]+")
 				&& password.length() > 7) {
 
 			validPassword = true;
@@ -110,17 +127,23 @@ public class User {
 
 	}
 	
+	/**
+	 * 
+	 * @param String username
+	 * @param String password
+	 * @return boolean true: logged in false: username and/or password
+	 */
+
 	public boolean logIn(String username, String password) {
-		
+
 		boolean logged = false;
-		
-		if(username.matches(this.username) && password.matches(this.password))
-			
+
+		if (username.matches(this.username) && password.matches(this.password))
+
 			logged = true;
-		
-		
+
 		return logged;
-		
+
 	}
 
 }
